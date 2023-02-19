d96da724810d1996e38d59bef59c537ddc1e3bd6
Dany Dubé <74633244+danydube1971@users.noreply.github.com>
2023-02-18 19:28:09 -0500

    Update Serveur_fichiers_local.py

--- Serveur_fichiers_local.py
+++ Serveur_fichiers_local.py
@@ -5,13 +5,12 @@ au serveur en utilisant l'adresse IP 10.0.0.236 et télécharger les fichiers de
 Assurez-vous d'installer le module Flask en exécutant la commande suivante dans votre terminal:
 pip3 install Flask
 
-Ce script crée un serveur Flask qui affiche une liste de fichiers dans le dossier courant en ordre alphabétique. Les utilisateurs peuvent 
-télécharger les fichiers en cliquant sur les liens correspondants.
+Ce script crée un serveur Flask qui affiche une liste de fichiers dans le dossier courant et sous-dossiers en ordre alphabétique. 
+Les utilisateurs peuvent télécharger les fichiers en cliquant sur les liens correspondants.
 
 Pour exécuter le script, ouvrez un terminal, naviguez jusqu'au répertoire contenant le fichier "serveur.py", et exécutez la commande suivante:
 
 Le serveur sera alors lancé et sera accessible à l'adresse http://10.0.0.236:5000/ depuis n'importe quel navigateur web sur le réseau local.'''
-
 
 import os
 from flask import Flask, send_file
@@ -20,17 +19,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def file_list():
-    file_list = os.listdir('.')
+    file_list = []
+    for root, dirs, files in os.walk('.'):
+        for f in files:
+            file_list.append(os.path.join(root, f))
     file_list.sort()
     file_links = ''
     for f in file_list:
         file_links += f'<p><a href="/download/{f}">{f}</a></p>'
     return file_links
 
-@app.route('/download/<filename>')
+@app.route('/download/<path:filename>')
 def download_file(filename):
     return send_file(filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(host='10.0.0.236')
+
 
